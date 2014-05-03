aacf824401797de9361f9ab2f90dfb6d7358825d
Glenn Yonemitsu <yonemitsu@gmail.com>
2014-05-03 14:27:55 -0400

    basic routes and static files support

--- spray.py
+++ spray.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python
 
 from __future__ import print_function
+import argparse
+import mimetypes
 import os.path
 import sys
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import yaml
 
 
@@ -17,14 +19,9 @@ with open(yaml_file) as fh:
     conf = yaml.load(fh)
 
 
-def create_view(template):
-    def view():
-        return render_template(template)
-    return view
-
-
 app = Flask(__name__)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
+
 for route, meta in conf.iteritems():
     if type(meta) is dict:
         template = meta['template']
@@ -32,8 +29,15 @@ for route, meta in conf.iteritems():
     else:
         template = meta
         name = route
-    view = create_view(template)
-    app.add_url_rule(route, name, view)
+    app.add_url_rule(route, name, lambda: render_template(template))
+
+def catchall_route(path):
+    static_file = os.path.abspath(os.path.join('static', path))
+    if not os.path.exists(static_file):
+        return '404'
+    return app.send_static_file(path)
+
+app.add_url_rule('/<path:path>', 'catchall', catchall_route)
 
 
 app.run(debug=True, host='0.0.0.0')
